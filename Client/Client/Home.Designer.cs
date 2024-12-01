8c6d9b45258948e2cdbd74c279c48a87e200e4da
cHienNezz <vochien1307@gmail.com>
2024-12-01 15:53:32 +0700

    Thiết kế UI
    Thiết kế giao diện cơ bản

---
+++ Client/Client/Home.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Client
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            label1 = new Label();
+            btnHome = new Button();
+            btnSchedule = new Button();
+            btnTicketSearch = new Button();
+            btnContact = new Button();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            chkboxOneway = new CheckBox();
+            chkboxRoundtrip = new CheckBox();
+            label2 = new Label();
+            label3 = new Label();
+            cmbBoxDeparture = new ComboBox();
+            cmbBoxDestination = new ComboBox();
+            label4 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            btnSearch = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(1, -3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1441, 77);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1211, 82);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(43, 43);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(1260, 93);
+            label1.Name = "label1";
+            label1.Size = new Size(124, 23);
+            label1.TabIndex = 3;
+            label1.Text = "Tên đăng nhập";
+            // 
+            // btnHome
+            // 
+            btnHome.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnHome.Location = new Point(309, 148);
+            btnHome.Name = "btnHome";
+            btnHome.Size = new Size(199, 52);
+            btnHome.TabIndex = 4;
+            btnHome.Text = "Trang chủ";
+            btnHome.UseVisualStyleBackColor = true;
+            // 
+            // btnSchedule
+            // 
+            btnSchedule.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSchedule.Location = new Point(514, 148);
+            btnSchedule.Name = "btnSchedule";
+            btnSchedule.Size = new Size(199, 52);
+            btnSchedule.TabIndex = 5;
+            btnSchedule.Text = "Lịch trình";
+            btnSchedule.UseVisualStyleBackColor = true;
+            // 
+            // btnTicketSearch
+            // 
+            btnTicketSearch.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnTicketSearch.Location = new Point(719, 148);
+            btnTicketSearch.Name = "btnTicketSearch";
+            btnTicketSearch.Size = new Size(199, 52);
+            btnTicketSearch.TabIndex = 6;
+            btnTicketSearch.Text = "Tra cứu vé";
+            btnTicketSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnContact
+            // 
+            btnContact.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnContact.Location = new Point(924, 148);
+            btnContact.Name = "btnContact";
+            btnContact.Size = new Size(199, 52);
+            btnContact.TabIndex = 7;
+            btnContact.Text = "Hỗ trợ";
+            btnContact.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(299, 139);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(835, 69);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 8;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(230, 230);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(977, 268);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 9;
+            pictureBox4.TabStop = false;
+            // 
+            // chkboxOneway
+            // 
+            chkboxOneway.AutoSize = true;
+            chkboxOneway.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            chkboxOneway.ForeColor = Color.Black;
+            chkboxOneway.Location = new Point(299, 263);
+            chkboxOneway.Name = "chkboxOneway";
+            chkboxOneway.Size = new Size(122, 32);
+            chkboxOneway.TabIndex = 10;
+            chkboxOneway.Text = "Một chiều";
+            chkboxOneway.UseVisualStyleBackColor = true;
+            // 
+            // chkboxRoundtrip
+            // 
+            chkboxRoundtrip.AutoSize = true;
+            chkboxRoundtrip.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            chkboxRoundtrip.ForeColor = Color.Black;
+            chkboxRoundtrip.Location = new Point(448, 263);
+            chkboxRoundtrip.Name = "chkboxRoundtrip";
+            chkboxRoundtrip.Size = new Size(102, 32);
+            chkboxRoundtrip.TabIndex = 11;
+            chkboxRoundtrip.Text = "Khứ hồi";
+            chkboxRoundtrip.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.2F);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(299, 319);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 23);
+            label2.TabIndex = 12;
+            label2.Text = "Điểm đi";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 10.2F);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(605, 319);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 23);
+            label3.TabIndex = 13;
+            label3.Text = "Điểm đến";
+            // 
+            // cmbBoxDeparture
+            // 
+            cmbBoxDeparture.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cmbBoxDeparture.FormattingEnabled = true;
+            cmbBoxDeparture.Items.AddRange(new object[] { "An Giang", "Bà Rịa-Vũng Tàu", "Bắc Giang", "Bắc Kạn", "Bạc Liêu", "Bắc Ninh", "Bến Tre", "Bình Định", "Bình Dương", "Bình Phước", "Bình Thuận", "Cà Mau", "Cần Thơ", "Cao Bằng", "Đà Nẵng", "Đắk Lắk", "Đắk Nông", "Điện Biên", "Đồng Nai", "Đồng Tháp", "Gia Lai", "Hà Giang", "Hà Nam", "Hà Nội", "Hà Tĩnh", "Hải Dương", "Hải Phòng", "Hậu Giang", "TP. Hồ Chí Minh", "Hòa Bình", "Hưng Yên", "Khánh Hòa", "Kiên Giang", "Kon Tum", "Lai Châu", "Lâm Đồng", "Lạng Sơn", "Lào Cai", "Long An", "Nam Định", "Nghệ An", "Ninh Bình", "Ninh Thuận", "Phú Thọ", "Phú Yên", "Quảng Bình", "Quảng Nam", "Quảng Ngãi", "Quảng Ninh", "Quảng Trị", "Sóc Trăng", "Sơn La", "Tây Ninh", "Thái Bình", "Thái Nguyên", "Thanh Hóa", "Thừa Thiên - Huế", "Tiền Giang", "Trà Vinh", "Tuyên Quang", "Vĩnh Long", "Vĩnh Phúc", "Yên Bái" });
+            cmbBoxDeparture.Location = new Point(299, 342);
+            cmbBoxDeparture.Name = "cmbBoxDeparture";
+            cmbBoxDeparture.Size = new Size(251, 36);
+            cmbBoxDeparture.TabIndex = 14;
+            // 
+            // cmbBoxDestination
+            // 
+            cmbBoxDestination.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cmbBoxDestination.FormattingEnabled = true;
+            cmbBoxDestination.Items.AddRange(new object[] { "An Giang", "Bà Rịa-Vũng Tàu", "Bắc Giang", "Bắc Kạn", "Bạc Liêu", "Bắc Ninh", "Bến Tre", "Bình Định", "Bình Dương", "Bình Phước", "Bình Thuận", "Cà Mau", "Cần Thơ", "Cao Bằng", "Đà Nẵng", "Đắk Lắk", "Đắk Nông", "Điện Biên", "Đồng Nai", "Đồng Tháp", "Gia Lai", "Hà Giang", "Hà Nam", "Hà Nội", "Hà Tĩnh", "Hải Dương", "Hải Phòng", "Hậu Giang", "TP. Hồ Chí Minh", "Hòa Bình", "Hưng Yên", "Khánh Hòa", "Kiên Giang", "Kon Tum", "Lai Châu", "Lâm Đồng", "Lạng Sơn", "Lào Cai", "Long An", "Nam Định", "Nghệ An", "Ninh Bình", "Ninh Thuận", "Phú Thọ", "Phú Yên", "Quảng Bình", "Quảng Nam", "Quảng Ngãi", "Quảng Ninh", "Quảng Trị", "Sóc Trăng", "Sơn La", "Tây Ninh", "Thái Bình", "Thái Nguyên", "Thanh Hóa", "Thừa Thiên - Huế", "Tiền Giang", "Trà Vinh", "Tuyên Quang", "Vĩnh Long", "Vĩnh Phúc", "Yên Bái" });
+            cmbBoxDestination.Location = new Point(605, 342);
+            cmbBoxDestination.Name = "cmbBoxDestination";
+            cmbBoxDestination.Size = new Size(251, 36);
+            cmbBoxDestination.TabIndex = 15;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 10.2F);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(908, 319);
+            label4.Name = "label4";
+            label4.Size = new Size(69, 23);
+            label4.TabIndex = 16;
+            label4.Text = "Ngày đi";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(908, 347);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 17;
+            // 
+            // btnSearch
+            // 
+            btnSearch.BackColor = Color.Chocolate;
+            btnSearch.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSearch.ForeColor = Color.White;
+            btnSearch.Location = new Point(623, 424);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(207, 50);
+            btnSearch.TabIndex = 18;
+            btnSearch.Text = "Tìm chuyến xe";
+            btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // Home
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1440, 723);
+            Controls.Add(btnSearch);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label4);
+            Controls.Add(cmbBoxDestination);
+            Controls.Add(cmbBoxDeparture);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(chkboxRoundtrip);
+            Controls.Add(chkboxOneway);
+            Controls.Add(pictureBox4);
+            Controls.Add(btnContact);
+            Controls.Add(btnTicketSearch);
+            Controls.Add(btnSchedule);
+            Controls.Add(btnHome);
+            Controls.Add(label1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBox3);
+            ForeColor = Color.Chocolate;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Home";
+            Text = "Home";
+            TransparencyKey = Color.PapayaWhip;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Button btnHome;
+        private Button btnSchedule;
+        private Button btnTicketSearch;
+        private Button btnContact;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private CheckBox chkboxOneway;
+        private CheckBox chkboxRoundtrip;
+        private Label label2;
+        private Label label3;
+        private ComboBox cmbBoxDeparture;
+        private ComboBox cmbBoxDestination;
+        private Label label4;
+        private DateTimePicker dateTimePicker1;
+        private Button btnSearch;
+    }
+}
---
+++ Client/Client/SignUp.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Client
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignUp));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            txtLoginName = new TextBox();
+            label2 = new Label();
+            txtPhoneNumber = new TextBox();
+            label3 = new Label();
+            txtEmail = new TextBox();
+            label4 = new Label();
+            txtPassword = new TextBox();
+            label5 = new Label();
+            txtConfirmPassword = new TextBox();
+            label6 = new Label();
+            btnSignUp = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(-1, -3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(862, 77);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(147, 184);
+            label1.Name = "label1";
+            label1.Size = new Size(110, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Tên đăng nhập:";
+            // 
+            // txtLoginName
+            // 
+            txtLoginName.Location = new Point(304, 181);
+            txtLoginName.Name = "txtLoginName";
+            txtLoginName.Size = new Size(356, 27);
+            txtLoginName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.DarkOrange;
+            label2.Location = new Point(349, 101);
+            label2.Name = "label2";
+            label2.Size = new Size(168, 54);
+            label2.TabIndex = 4;
+            label2.Text = "Đăng kí";
+            // 
+            // txtPhoneNumber
+            // 
+            txtPhoneNumber.Location = new Point(304, 244);
+            txtPhoneNumber.Name = "txtPhoneNumber";
+            txtPhoneNumber.Size = new Size(356, 27);
+            txtPhoneNumber.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(147, 247);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 20);
+            label3.TabIndex = 5;
+            label3.Text = "SĐT:";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(304, 309);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(356, 27);
+            txtEmail.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(147, 312);
+            label4.Name = "label4";
+            label4.Size = new Size(49, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Email:";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(304, 368);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(356, 27);
+            txtPassword.TabIndex = 10;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(147, 371);
+            label5.Name = "label5";
+            label5.Size = new Size(73, 20);
+            label5.TabIndex = 9;
+            label5.Text = "Mật khẩu:";
+            // 
+            // txtConfirmPassword
+            // 
+            txtConfirmPassword.Location = new Point(304, 434);
+            txtConfirmPassword.Name = "txtConfirmPassword";
+            txtConfirmPassword.Size = new Size(356, 27);
+            txtConfirmPassword.TabIndex = 12;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(147, 437);
+            label6.Name = "label6";
+            label6.Size = new Size(137, 20);
+            label6.TabIndex = 11;
+            label6.Text = "Xác nhận mật khấu:";
+            // 
+            // btnSignUp
+            // 
+            btnSignUp.Location = new Point(386, 500);
+            btnSignUp.Name = "btnSignUp";
+            btnSignUp.Size = new Size(94, 29);
+            btnSignUp.TabIndex = 13;
+            btnSignUp.Text = "Đăng kí";
+            btnSignUp.UseVisualStyleBackColor = true;
+            // 
+            // SignUp
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(849, 541);
+            Controls.Add(btnSignUp);
+            Controls.Add(txtConfirmPassword);
+            Controls.Add(label6);
+            Controls.Add(txtPassword);
+            Controls.Add(label5);
+            Controls.Add(txtEmail);
+            Controls.Add(label4);
+            Controls.Add(txtPhoneNumber);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(txtLoginName);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "SignUp";
+            Text = "SignUp";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TextBox txtLoginName;
+        private Label label2;
+        private TextBox txtPhoneNumber;
+        private Label label3;
+        private TextBox txtEmail;
+        private Label label4;
+        private TextBox txtPassword;
+        private Label label5;
+        private TextBox txtConfirmPassword;
+        private Label label6;
+        private Button btnSignUp;
+    }
+}
