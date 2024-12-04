5a40f393d82a4788da565f19c8f0bbbbe0694bab
namelessideal1005 <86345837+namelessideal1005@users.noreply.github.com>
2024-12-04 21:35:51 +0700

    save the world

--- Server/ConnectDB.py
+++ Server/ConnectDB.py
@@ -48,12 +48,13 @@ def JSONOutput(jsondata) -> str:
 
 class ConnectDB:
 
-    def __init__(self):
-        self.Connector = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
+    #def __init__(self):
+    Connector = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};"
                                 "Server=localhost;"
                                 "Database=bus_server_prod;"
                                 "Trusted_Connection=yes;")
     
+    @staticmethod
     def GetUserName(self, usr: str, pwd: str) -> dict:
         try:
             Cursor = self.Connector.cursor()
@@ -71,16 +72,19 @@ class ConnectDB:
 
         return {"UserID": row[0], "Username": row[1], "Full Name": row[3], "Phone": row[4], "Email": row[5]}
     
-    def GetTrip(self, depart: int, arrive: int, date: str) -> list:
+    #@staticmethod
+    def GetTrip(self, depart: str, arrive: str, departdate: str, returndate: str) -> list:
         listTrips = []
         try:
             Cursor = self.Connector.cursor()
-            Cursor.execute(f"""SELECT TripId, TripName, Depart.LocationName, Arrive.LocationName, DepartTime, TripStatusName, PlateNumber FROM Trips
-                            INNER JOIN Cars ON Trips.CarId = Cars.CarID
-                            INNER JOIN Locations AS Depart ON Trips.DepartLocation = Depart.LocationId
-                            INNER JOIN Locations AS Arrive ON Trips.ArriveLocation = Arrive.LocationId
-                            INNER JOIN TripStatus ON Trips.TripStatusId = TripStatus.TripStatusId
-                            WHERE DepartLocation = {depart} AND ArriveLocation = {arrive} AND DepartTime = '{date}'""")
+            Cursor.execute(f"""SELECT TripId, TripName, DepartLocation, ArriveLocation, DepartTime,TripStatusId,PlateNumber FROM 
+                                (SELECT * FROM Trips WHERE DepartLocation = '{depart}' AND ArriveLocation = '{arrive}' AND DepartTime = '{departdate}'
+                                UNION
+                                SELECT * FROM Trips WHERE DepartLocation = '{arrive}' AND ArriveLocation = '{depart}' AND DepartTime = '{returndate}')
+                                AS AllTrips
+                                INNER JOIN Cars ON AllTrips.CarId = Cars.CarId""")
+            #Might need to add ReturnDate as well           
+            
             if Cursor.rowcount == 0:
                 print('Trip not found')
                 return []
@@ -95,6 +99,7 @@ class ConnectDB:
                 listTrips.append({"TripID": row[0], "TripName": row[1], "DepartLocation": row[2], "ArrivalLocation": row[3], "DepartureDate": row[4].strftime('%d/%m/%Y %H:%M:%S'), "Status": row[5], "Plate": row[6]})
         return listTrips
     
+    #@staticmethod
     def GetTicket(self, username: str, ticketid: int) -> list:
         listTickets = []
         try:
@@ -117,8 +122,36 @@ class ConnectDB:
             for row in rows:
                 listTickets.append({"TicketID": row[0], "FullName": row[1], "TripName": row[2], "DepartTime": row[3].strftime('%d/%m/%Y %H:%M:%S'), "SeatNumber": row[4], "TicketType": row[5], "TotalPrice": int(row[6])})
         return listTickets
-        pass
+        
+
+    def GetFeedBack(self, fbid: int):
+        try:
+            Cursor = self.Connector.cursor()
+            Cursor.execute(f"SELECT FeedbackId, UserName, Description_ FROM UserFeedback INNER JOIN Users ON UserFeedback.UserId = Users.UserId WHERE FeedbackId = {fbid}")
+            if Cursor.rowcount == 0:
+                print('Feedback not found')
+                return ()
+            else:
+                print('Success')
+        except Exception as e:
+            print(str(e))
+            return ()
+        
+        row = Cursor.fetchone()
+
+        return {"FeedbackID": row[0], "UserName": row[1], "Content": row[2]}
     
 
 
-connector = ConnectDB()
+    
+
+
+#connector = ConnectDB()
+
+#print(connector.GetUserName('abc','123'))
+
+
+
+
+#y = JSONOutput(connector.GetTrip('B', 'A', '2024-06-20', '2024-01-10'))
+#print(y)
---
+++ Server/testfastapi.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, Query, HTTPException
+from typing import Optional
+import ConnectDB
+import uvicorn
+from pydantic import BaseModel
+import sv_model
+
+app = FastAPI()
+
+
+
+#@app.post("/register")
+
+
+@app.get("/auth/login/{usr}")
+async def Login(usr: str, pwd: str):
+    print(usr + " " + pwd)
+    return ConnectDB.ConnectDB.GetUserName(ConnectDB.ConnectDB, usr, pwd)
+    
+        
+
+
+#@app.get("/trips")
+
+
+@app.get("/trips/{date}")
+async def GetTrip(date: str, dpt: int, arr: int):
+    return ConnectDB.ConnectDB.GetTrip(ConnectDB.ConnectDB, dpt, arr, date)
+
+
+
+@app.get("/trips")
+async def GetAllTrip(
+    from_location: str = Query(..., alias="from", description="Departure location"),
+    to_location: str = Query(..., alias="to", description="Arrival location"),
+    from_time: str = Query(..., alias="fromTime", description="Departure time"),
+    to_time: Optional[str] = Query(None, alias="toTime", description="Return time"),
+    is_return: Optional[bool] =  Query(False, description="Indicates if the ticket is round-trip"),
+    ticket_count: Optional[int] = Query(..., ge=1, description="Number of tickets to book")
+):
+    return ConnectDB.ConnectDB.GetTrip(ConnectDB.ConnectDB, from_location, to_location, from_time, to_time)
+
+
+
+
+@app.get("/tickets/{usr}")
+async def GetTicket(usr: str, tick: int):
+    return ConnectDB.ConnectDB.GetTicket(ConnectDB.ConnectDB, usr, tick)
+
+#@app.get("/trips/{trip_id}/seats")
+
+
+#@app.get("")
+
+
+
+#if __name__ == "__main__":
+#    uvicorn.run(app, host = "127.0.0.1", port = 8000)
