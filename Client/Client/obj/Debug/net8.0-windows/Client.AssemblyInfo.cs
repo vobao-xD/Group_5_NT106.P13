76c64700170260be54a0845d01702f67e88da619
namelessideal1005 <86345837+namelessideal1005@users.noreply.github.com>
2024-12-25 21:31:02 +0700

    fix port

--- Client/Client/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
+++ Client/Client/obj/Debug/net8.0-windows/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0f64817ba1c7e16d087698b8b45295953c2af87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a2d73a54970bac38fbbeb60dca015a7668a144")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
