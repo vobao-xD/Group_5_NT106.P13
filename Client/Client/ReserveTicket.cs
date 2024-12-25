8007aa0724a4afa8202efd7c2497ff26dd7db774
Võ Quốc Bảo <vobao8682@gmail.com>
2024-12-25 22:32:19 +0700

    Finish Book Seat

--- Client/Client/ReserveTicket.cs
+++ Client/Client/ReserveTicket.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Net.Http;
 using Newtonsoft.Json;
 using Client.Model;
+using System.Diagnostics;
 
 namespace Client
 {
@@ -26,19 +27,13 @@ namespace Client
             //GET /seats?tripid={trip.TripID.ToString}
             TripID = trip.TripId;
             BusID = trip.BusId;
-
             this.plate = trip.Plate;
             _authToken = authToken; // dùng biến auth token này để tiếp tục làm việc
             _userInfo = userInfo;
 
         }
         List<Seat>? seatsList;
-        List<string> items = new List<string>
-        {
-            "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10",
-            "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10",
-            "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10"
-        };
+        List<string> items;
         string? plate;
         public async Task GetUnavailableSeat(string? plate)
         {
@@ -70,31 +65,35 @@ namespace Client
 
         private async void ReserveTicket_Load(object sender, EventArgs e)
         {
+            items = new List<string>
+            {
+            "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10",
+            "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10",
+            "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10"
+            };
             try
             {
                 HttpClient client = new();
                 client.BaseAddress = new Uri($"http://127.0.0.1:8002/");
-                MessageBox.Show($"http://127.0.0.1:8002/seats?busid={BusID}&isbook=1");
+                //MessageBox.Show($"http://127.0.0.1:8002/seats?busid={BusID}&isbook=1");
                 HttpResponseMessage response = await client.GetAsync($"seats?busid={BusID}&isbook=1");
                 string seats = await response.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
                 MessageBox.Show(seats);
                 seatsList = JsonConvert.DeserializeObject<List<Seat>>(seats);
-                if (seatsList == null) { return; }
-                List<string> unseat = [];
-                foreach (Seat seat in seatsList) {
-                    unseat.Add(seat.GetSeatName());
-                }
-                foreach (string seat in unseat) {
-                    items.Remove(seat);
-                }
-                checkedListBox1.BeginUpdate();
-                checkedListBox1.Items.AddRange(items.ToArray());
-                foreach (string u in unseat)
+                //foreach (Seat seat in seatsList) {
+                //    unseat.Add(seat.GetSeatName());
+                //}
+                //items.RemoveAll(a => newSeatsList.Contains(a.ToString()));
+                //List<string> toRemove = new List<string>();
+
+                foreach (Seat seat in seatsList)
                 {
-                    checkedListBox1.Items.Remove(u);
+                    if (seat.SeatName != null) items.Remove(seat.SeatName);
                 }
-                checkedListBox1.EndUpdate();
-                
+
+                checkedListBox1.Items.Clear();
+                checkedListBox1.Items.AddRange(items.ToArray());
+
             }
             catch (Exception ex)
             {
@@ -104,16 +103,9 @@ namespace Client
     }
     class Seat
     {
-        string? Plate;
-        string? SeatName;
-        bool IsBook;
-        int SeatID;
-        public string? GetSeatName()
-        {
-            if (SeatName != null) return SeatName;
-            else return "";
-        }
-        
-
+        public string? Plate;
+        public string? SeatName;
+        public bool IsBook;
+        public int SeatID;
     }
 }
