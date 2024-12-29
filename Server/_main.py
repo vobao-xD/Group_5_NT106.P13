9541693102ab9a64c607fbcb37c4e2227fd205c4
Võ Quốc Bảo <vobao8682@gmail.com>
2024-12-30 01:11:47 +0700

    Thêm tính năng chạy trong mạng cục bộ, merge API forget passwordpassword

--- Server/_main.py
+++ Server/_main.py
@@ -1,5 +1,6 @@
 from Controllers import *
+from Ultilities import ServerIP
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8002)
+    uvicorn.run(app, host=ServerIP, port=8002)
